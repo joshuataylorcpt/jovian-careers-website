3ed3f7234772f6bd9d1d673df2032293f5469a3d
joshuataylorcpt <131142210+joshuataylorcpt@users.noreply.github.com>
2024-12-13 11:05:59 +0000

    Looks pretty, good start
    Needs a lot of work, just the landing page

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #need to create an app, object of the type class
 
 app = Flask(__name__)
 
 @app.route('/') #this is the empty route
-def hello_world():
-    return 'Hello, world!'
+def hello_jovian():
+    return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
