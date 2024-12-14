68094995aea850d22bb100f7d76e2f1e26b24606
joshuataylorcpt <131142210+joshuataylorcpt@users.noreply.github.com>
2024-12-14 13:13:53 +0000

    updated ui

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 #need to create an app, object of the type class
 
 app = Flask(__name__)
 
 @app.route('/') #this is the empty route
-def hello_jovian():
+def home():
     return render_template('home.html')
 
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+    
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
